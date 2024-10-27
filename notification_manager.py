a766602aba2eda28583bc24692df0fe941bf866c
bazangra <bharat.angra3@gmail.com>
2024-10-27 13:28:56 +0000

    Update notification_manager.py

--- notification_manager.py
+++ notification_manager.py
@@ -1,8 +1,8 @@
 import requests
 from twilio.rest import Client
 
-account_sid = 'ACbbb8fba945900aa7f2a1f4116d1b4ab3'
-auth_token = '3a82e7c3b683c8f2d3bd6f80ad1cc50c'
+account_sid = []
+auth_token = []
 
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
@@ -12,7 +12,7 @@ class NotificationManager:
     def send_text(self, price, origin_airport, destination_airport, out_date, return_date):
         client = Client(account_sid, auth_token)
         message = client.messages.create(
-            from_='whatsapp:+14155238886',
+            from_='whatsapp:+[]',
             body=f'Low price alert! Only £{price} to fly from {origin_airport} to {destination_airport}, on {out_date} until {return_date}.',
-            to='whatsapp:+447711259832'
+            to='whatsapp:+[]'
         )
